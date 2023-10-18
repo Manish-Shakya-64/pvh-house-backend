b8f977374655c8a75f45267aa9f69c5ff93a41ba
Manish-Shakya-64 <100149882+Manish-Shakya-64@users.noreply.github.com>
2023-10-18 23:53:08 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,6 +9,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure:false,
   };
 
   res.status(statusCode).cookie('token', token, option).json({
